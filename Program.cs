4341f4b6e0724c94091cfa9a1b60c0cd8f725be3
Claudiomiro Marques <marques_55@hotmail.com>
2023-01-04 22:12:29 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+
+// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Console.WriteLine("Teste do Smartphone nokia");
+Smartphone nokia = new Nokia(numero: "95555-6666", modelo: "model", imei: "123456789", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("telegram");
+
+
+Console.WriteLine("Teste do Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "88888-0101", modelo: "model", imei: "123456789", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("kwai");
